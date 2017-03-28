0c37f4ad77a271f2cefab2dacb111689de7ae2aa
JFYVART <jean.francois.yvart@gmail.com>
2017-03-28 13:42:43 +0200

    Version testée du nouveau protocole de communication

--- src/com/cfranc/irc/IfClientServerProtocol.java
+++ src/com/cfranc/irc/IfClientServerProtocol.java
@@ -3,67 +3,107 @@ package com.cfranc.irc;
 public class IfClientServerProtocol {
 	public static final String LOGIN_PWD = "#Login?#Pwd?";
 	public static final String SEPARATOR = "#";
+	
+	/**
+	 *  Liste des commandes utilisables
+	 */
+	// Pb technique ou refus d'une action demandée
 	public static final String KO = "#KO";
+	// Acceptation d'une action demandée
 	public static final String OK = "#OK";
+	// Un nouvel utilisateur arrive dans un salon
 	public static final String ADD = "#+#";
+	// Un utilisateur quitte un salon 
 	public static final String DEL = "#-#";
-	public static final String NVELLESESSION = "#NVS#";
+	// Un utilisateur demande la création d'un nouveau salon
+	public static final String NVSALON = "#NVS#";
+	// Un utilisateur demande la création d'un msg privé
 	public static final String NVMSGPRIVE = "#MSGPRIVE#";
 
 	/**
 	 * Régle de communication : Tout msg doit avoir cette syntaxe
 	 * 
-	 * "#" +Nom utilisateur + "#" + Msg utilisateur +"#" + commande utilisateur
-	 * +"#" + IdSalon + "#" + Nom Salon
+	 * "#" + Login utilisateur + "#" + Password utilisateur + "#" + Msg utilisateur +"#" + commande utilisateur +"#" + IdSalon + "#" + Nom Salon
 	 * 
 	 * Exemples :
 	 * 
-	 * #Toto#Hello tout le monde##0#Salon général (Toto envoit le message
+	 * #Toto##Hello tout le monde##0#Salon général (Toto envoit le message
 	 * "Hello tout le monde" au salon n° 0 (Salon général))
 	 * 
-	 * #Toto##-#0#Salon général (Toto quitte le salon n° 0 (Salon général))
+	 * #Toto###-#0#Salon général (Toto quitte le salon n° 0 (Salon général))
 	 * 
-	 * #Toto#Donald#+MSG##Salon privé (Toto demande la création d'un nouveau
+	 * #Toto##Donald#+MSG##Salon privé (Toto demande la création d'un nouveau
 	 * salon fermé pour envoyer un message privé à Donald)
 	 * 
-	 * #Toto##+SAL## Salon Java (Toto demande la création d'un nouveau
-	 * salon public nommé "Salon Java")
+	 * #Toto###+SAL## Salon Java (Toto demande la création d'un nouveau salon
+	 * public nommé "Salon Java")
 	 * 
 	 * 
 	 **/
 
-	public static String decodeProtocoleLogin(String line) {
+	public static String decodeProtocole_Login(String line) {
 		String[] userMsg = line.split(IfClientServerProtocol.SEPARATOR);
 		String login = userMsg[1];
+		if (login == null)
+			login = "";
 		return login;
 	}
 
-	public static String decodeProtocoleMessage(String line) {
+	public static String decodeProtocole_PWD(String line) {
 		String[] userMsg = line.split(IfClientServerProtocol.SEPARATOR);
-		String msg = userMsg[2];
+		String pwd = userMsg[2];
+		if (pwd == null)
+			pwd = "";
+		return pwd;
+	}
+
+	public static String decodeProtocole_Message(String line) {
+		String[] userMsg = line.split(IfClientServerProtocol.SEPARATOR);
+		String msg = userMsg[3];
+		if (msg == null)
+			msg = "";
 		return msg;
 	}
 
-	public static String decodeProtocoleCommand(String line) {
+	public static String decodeProtocole_Command(String line) {
 		String[] userMsg = line.split(IfClientServerProtocol.SEPARATOR);
 		String command = userMsg[4];
+		if (command == null)
+			command = "";
 		return command;
 	}
 
-	public static int decodeProtocoleIdSalon(String line) {
+	public static int decodeProtocole_IdSalon(String line) {
 		String[] userMsg = line.split(IfClientServerProtocol.SEPARATOR);
-		String idSalonString = userMsg[6];
+		String idSalonString = userMsg[5];
+		if (idSalonString == null)
+			idSalonString = "0";
 		return Integer.parseInt(idSalonString);
 	}
 
-	public static String decodeProtocoleNomSalon(String line) {
+	public static String decodeProtocole_NomSalon(String line) {
 		String[] userMsg = line.split(IfClientServerProtocol.SEPARATOR);
-		String salonName = userMsg[7];
+		String salonName = userMsg[6];
+		if (salonName == null)
+			salonName = "";
 		return salonName;
 	}
 
-	
-	public static String encodeLigne(String login, String msg, String command, int salonId, String salonName){
-		return SEPARATOR + login + SEPARATOR + msg + SEPARATOR + command + SEPARATOR +  salonId + SEPARATOR + salonName;
+	public static String encodeProtocole_Ligne(String login, String pwd, String msg, String command, int salonId,
+			String salonName) {
+		// On remplace les NULL par des chaines vides
+		if (login == null)
+			login = "";
+		if (pwd == null)
+			pwd = "";
+		if (msg == null)
+			msg = "";
+		if (command == null)
+			command = "";
+		if (salonName == null)
+			salonName = "";
+		// Construction de la ligne
+		return SEPARATOR + login + SEPARATOR + pwd + SEPARATOR + msg + SEPARATOR + command + SEPARATOR + salonId
+				+ SEPARATOR + salonName;
 	}
 }
---
+++ src/com/cfranc/irc/TstClientServerProtocole.java
@@ -0,0 +1,149 @@
+package com.cfranc.irc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TstClientServerProtocole {
+
+	@Test
+	public void test_encodeProtocole_Ligne_Login() {
+		String line = "";
+		String login ="Toto";
+		String pwd = "";  
+		String msg = "";
+		String command = "";
+		int salonId = 0;
+		String salonName = "";
+		
+		line = IfClientServerProtocol.encodeProtocole_Ligne(login, pwd, msg, command, salonId, salonName);
+		assertEquals(login,IfClientServerProtocol.decodeProtocole_Login(line));
+	}
+	
+	@Test
+	public void test_encodeProtocole_Ligne_pwd() {
+		String line = "";
+		String login ="";
+		String pwd = "pvd12345";  
+		String msg = "";
+		String command = "";
+		int salonId = 0;
+		String salonName = "";
+		
+		line = IfClientServerProtocol.encodeProtocole_Ligne(login, pwd, msg, command, salonId, salonName);
+		assertEquals(pwd,IfClientServerProtocol.decodeProtocole_PWD(line));
+	}	
+	
+	@Test
+	public void test_encodeProtocole_Ligne_Msg() {
+		String line = "";
+		String login ="";
+		String pwd = "";  
+		String msg = "Hello World !!!";
+		String command = "";
+		int salonId = 0;
+		String salonName = "";
+		
+		line = IfClientServerProtocol.encodeProtocole_Ligne(login, pwd, msg, command, salonId, salonName);
+		assertEquals(msg,IfClientServerProtocol.decodeProtocole_Message(line));
+	}
+	
+	@Test
+	public void test_encodeProtocole_Ligne_Command() {
+		String line = "";
+		String login ="";
+		String pwd = "";  
+		String msg = "";
+		String command = "-";
+		int salonId = 0;
+		String salonName = "";
+		
+		line = IfClientServerProtocol.encodeProtocole_Ligne(login, pwd, msg, command, salonId, salonName);
+		assertEquals(command,IfClientServerProtocol.decodeProtocole_Command(line));
+	}
+	
+	@Test
+	public void test_encodeProtocole_Ligne_SalonId() {
+		String line = "";
+		String login ="";
+		String pwd = "";  
+		String msg = "";
+		String command = "";
+		int salonId = 6;
+		String salonName = "";
+		
+		line = IfClientServerProtocol.encodeProtocole_Ligne(login, pwd, msg, command, salonId, salonName);
+		assertEquals(salonId,IfClientServerProtocol.decodeProtocole_IdSalon(line));
+	}	
+	
+	@Test
+	public void test_encodeProtocole_Ligne_SalonName() {
+		String line = "";
+		String login ="";
+		String pwd = "";  
+		String msg = "";
+		String command = "";
+		int salonId = 0;
+		String salonName = "Salon Général";
+		
+		line = IfClientServerProtocol.encodeProtocole_Ligne(login, pwd, msg, command, salonId, salonName);
+		assertEquals(salonName,IfClientServerProtocol.decodeProtocole_NomSalon(line));
+	}
+	
+	@Test
+	public void test_encodeProtocole_Ligne_Login_Vide() {
+		String line = "";
+		String login ="";
+		String pwd = "";  
+		String msg = "";
+		String command = "";
+		int salonId = 0;
+		String salonName = "";
+		
+		line = IfClientServerProtocol.encodeProtocole_Ligne(login, pwd, msg, command, salonId, salonName);
+		assertEquals(login,IfClientServerProtocol.decodeProtocole_Login(line));
+	}
+	
+	@Test
+	public void test_encodeProtocole_Ligne_Pwd_Vide() {
+		String line = "";
+		String login ="";
+		String pwd = "";  
+		String msg = "";
+		String command = "";
+		int salonId = 0;
+		String salonName = "";
+		
+		line = IfClientServerProtocol.encodeProtocole_Ligne(login, pwd, msg, command, salonId, salonName);
+		assertEquals(pwd,IfClientServerProtocol.decodeProtocole_PWD(line));
+	}	
+	
+	@Test
+	public void test_encodeProtocole_Ligne_Msg_Vide() {
+		String line = "";
+		String login ="";
+		String pwd = "";  
+		String msg = "";
+		String command = "";
+		int salonId = 0;
+		String salonName = "";
+		
+		line = IfClientServerProtocol.encodeProtocole_Ligne(login, pwd, msg, command, salonId, salonName);
+		assertEquals(msg,IfClientServerProtocol.decodeProtocole_Message(line));
+	}
+	
+	@Test
+	public void test_encodeProtocole_Ligne_Command_Vide() {
+		String line = "";
+		String login ="";
+		String pwd = "";  
+		String msg = "";
+		String command = "";
+		int salonId = 0;
+		String salonName = "";
+		
+		line = IfClientServerProtocol.encodeProtocole_Ligne(login, pwd, msg, command, salonId, salonName);
+		assertEquals(command,IfClientServerProtocol.decodeProtocole_Command(line));
+	}
+	
+}
